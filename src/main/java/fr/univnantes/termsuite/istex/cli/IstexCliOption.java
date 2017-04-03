24c27e4983d59feec049c23f208868872b9265ad
Damien Cram <damien.cram@univ-nantes.fr>
2017-04-03 17:41:02 +0200

    verison 1.1.1, upgraded to TermSuite 3.0.1

--- src/main/java/fr/univnantes/termsuite/istex/cli/IstexCliOption.java
+++ src/main/java/fr/univnantes/termsuite/istex/cli/IstexCliOption.java
@@ -1,26 +1,32 @@
 package fr.univnantes.termsuite.istex.cli;
 
+import java.util.Collections;
+import java.util.List;
+
 import fr.univnantes.termsuite.tools.opt.CliOption;
 import fr.univnantes.termsuite.tools.opt.OptType;
 
+@SuppressWarnings("unchecked")
 public enum IstexCliOption implements CliOption {
 	// GATHERER 
-	DOCUMENT_IDS("doc-id", 		null, 	OptType.T_STRING, 	"The \",\"-separated list of ids of ISTEX documents"),
-	ID_FILE("id-file", 		null, 	OptType.T_FILE, 	"A file containing the list of ISTEX document ids of the corpus"),
-	FULLTEXT("fulltext", 		null, 	OptType.T_NONE, 	"Runs on documents' fulltexts instead of abstract only" ),
-	FAIL_ON_MISSING("fail-on-missing", 		null, 	OptType.T_NONE, 	"Aborts the process when the fulltext of a document is not available" ),
+	DOCUMENT_IDS("doc-id", 		null, 	OptType.T_STRING, 	"The \",\"-separated list of ids of ISTEX documents", Collections.EMPTY_LIST),
+	ID_FILE("id-file", 		null, 	OptType.T_FILE, 	"A file containing the list of ISTEX document ids of the corpus", Collections.EMPTY_LIST),
+	FULLTEXT("fulltext", 		null, 	OptType.T_NONE, 	"Runs on documents' fulltexts instead of abstract only", Collections.EMPTY_LIST ),
+	FAIL_ON_MISSING("fail-on-missing", 		null, 	OptType.T_NONE, 	"Aborts the process when the fulltext of a document is not available", Collections.EMPTY_LIST ),
 	;
 	
 	private String optName;
 	private String optShortName;
 	private OptType argType;
 	private String description;
-	
-	private IstexCliOption(String optName, String optShortName, OptType argType, String description) {
+	private List<Object> allowedValues;
+
+	private IstexCliOption(String optName, String optShortName, OptType argType, String description, List<Object> allowedValues) {
 		this.optName = optName;
 		this.optShortName = optShortName;
 		this.argType = argType;
 		this.description = description;
+		this.allowedValues = allowedValues;
 	}
 
 	/* (non-Javadoc)
@@ -61,5 +67,10 @@ public enum IstexCliOption implements CliOption {
 	@Override
 	public String getDescription() {
 		return description;
+	}
+
+	@Override
+	public List<Object> getAllowedValues() {
+		return allowedValues;
 	}
 }
